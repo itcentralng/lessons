8dcda525f5e44e363d066fce3e1cefff848e0cc5
Nasir Mustapha <34660309+mrteey@users.noreply.github.com>
2022-02-22 11:48:14 +0100

    Create feb22.py

---
+++ feb22.py
@@ -0,0 +1,14 @@
+"""
+
+Q1: shoppinglist = ['cake', 'cashew', 'gum']
+    Using the above list, execute the following:
+    (a) Convert individual items in the list to capital letters such that when I call the 
+        list I get all the items like this ['CAKE', 'CASHEW', 'GUM']
+    (b) Switch the positions of 'gum' and 'cashew' such that when the list is called it returns ['cake', 'gum', 'cashew']
+
+Q2: ages = [10, 15, 16, 17. 95]
+    people = ['Hassan', 'Adama', 'Bishir', 'Saleh', 'Clement']
+    
+    using the ages and people list write a code that prints out each person with their appropraite age for example: 
+    'Hassan is 10 years old' should be printed in the case of Hassan and 10
+"""
