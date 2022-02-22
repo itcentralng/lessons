5f7144b54865c85710846dc4f650fbd4b2116936
kassandrahj <jonahkassandrah@gmail.com>
2022-02-22 23:43:48 +0100

    assignment submission

--- feb22.py
+++ feb22.py
@@ -12,3 +12,45 @@ Q2: ages = [10, 15, 16, 17. 95]
     using the ages and people list write a code that prints out each person with their appropraite age for example: 
     'Hassan is 10 years old' should be printed in the case of Hassan and 10
 """
+print('Kassandrah')
+
+from ssl import PEM_FOOTER
+
+
+shoppinglist = ['cake', 'cashew', 'gum']
+
+sl = shoppinglist[0]
+
+y= sl.swapcase()
+
+sl1 =shoppinglist[1]
+
+x = sl1.swapcase()
+
+sl2 = shoppinglist[2]
+
+z= sl2.swapcase()
+
+finallist = [y,x,z]
+
+print(finallist)
+
+finallist2 = [y,z,x]
+
+print(finallist2)
+
+
+# q2
+
+ages = [10, 15, 16, 17, 95]
+people = ['Hassan', 'Adama', 'Bishir', 'Saleh', 'Clement']
+# for age in ages
+print('{} is {} years old'.format(people[0],ages[0]))
+
+print('{} is {} years old'.format(people[1],ages[1]))
+
+print('{} is {} years old'.format(people[2],ages[2]))
+
+print('{} is {} years old'.format(people[3],ages[3]))
+
+print('{} is {} years old'.format(people[4],ages[4]))
