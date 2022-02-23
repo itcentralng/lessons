5847baaa5b3ee932c366c023c7e386e578c0ccea
Nasir Mustapha <34660309+mrteey@users.noreply.github.com>
2022-02-23 12:09:45 +0100

    Create feb23.py

---
+++ feb23.py
@@ -0,0 +1,12 @@
+"""
+Q1: Fruits = ['Apples', 'Oranges', 'Bananas', 'Cashews']
+    Using the fruits list perform the following operations:
+      (a) Use the append method to add the following to the end of the list:
+            Mangoes, Berries, Watermelons
+      (b) Use the insert method to add Guavas, Pineapples at index 5 and 3 respectively
+      
+      (c) Use the insert method to repeat all the fruits at random
+          indexes and find the index of all the last repeated items in the list
+
+Q2: List all the methods available in the list class with explanation and an example.
+"""
