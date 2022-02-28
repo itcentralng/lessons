d612eb7a31acb4521d4fae917348369595bea1ee
Nasir Mustapha <34660309+mrteey@users.noreply.github.com>
2022-02-28 11:57:40 +0100

    Create feb28.py

---
+++ feb28.py
@@ -0,0 +1,21 @@
+sentence = 'my Name is kabir, I am 15 years old.'
+q1 = "Fix sentence such that when I call sentence I get 'My name is Kabir, I am 15 years old.'"
+q2 = "Create two new variables name and age that hold the name and age of Kabir from the sentence such that when I call name I get 'Kabir' and when I call age I get '15'"
+
+# Muhammad
+"""Write your answer below here"""
+
+# Kabir
+"""Write your answer below here"""
+
+# Usman
+"""Write your answer below here"""
+
+# Kassandrah
+"""Write your answer below here"""
+
+# Amrah
+"""Write your answer below here"""
+
+# Jabir
+"""Write your answer below here"""
