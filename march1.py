b35ee993bc40b16f298b37604feeb0ad45b3a85d
kassandrahj <jonahkassandrah@gmail.com>
2022-03-02 00:22:14 +0100

    submission

--- march1.py
+++ march1.py
@@ -8,6 +8,10 @@ q1 = """
     """
 
 # Write your answer below this line
+print ('Kassandrah\n')
+
+js_shoppingcart =[] 
+print(f'Jabir, here is your cart:{js_shoppingcart}\n ')
 
 q2 = """
         Thank you for the empty shopping cart,
@@ -18,13 +22,31 @@ q2 = """
 
 # Write your answer below this line
 
+js_shoppingcart.append('cake')
+js_shoppingcart.append('cashew')
+js_shoppingcart.append('brownies')
+js_shoppingcart.append('zobo')
+print(f'your cart conatins {js_shoppingcart}\n' )
+
 q3 = """
     James is happy with his purchase and wants to checkout.
     Please modify his cart so that it shows the price and quantity
     of each item purchased.
     """
 
+
+
 # Write your answer below  this line
+
+prices = [100, 200, 300, 400]
+js_shoppingcart =['cake', 'cashew', 'brownies', 'zobo']
+quantity = [2,3,4,5]
+
+print(f'item 1 : {js_shoppingcart[0]}, the price is {prices[0]} and you got {quantity[0]} in total')
+print(f'item 2 : {js_shoppingcart[1]}, the price is {prices[1]} and you got {quantity[1]} in total')
+print(f'item 3 : {js_shoppingcart[2]}, the price is {prices[2]} and you got {quantity[2]} in total')
+print(f'item 4 : {js_shoppingcart[3]}, the price is {prices[3]} and you got {quantity[3]} in total\n')
+
 
 q4 = """
     Thank you for all the help dear programmer,
@@ -33,3 +55,16 @@ q4 = """
     """
 
 # Write your answer below this line
+
+total_price =sum(prices)
+
+total_quantity = sum(quantity)
+
+print(f'the total items purchased is {total_quantity} and the price is {total_price} naira\n')
+
+prices.clear()
+quantity.clear()
+js_shoppingcart.clear()
+
+print(f'now your cart is empty and ready for the next operation Jabir: {js_shoppingcart},prices:{prices} and quantity: {quantity}\n')
+
