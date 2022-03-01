28a2ee39ea2719b9e90f03f2d94ad36c3ddded26
Usman Abba Usman cbk <61090729+usmanabbacbk@users.noreply.github.com>
2022-03-02 00:35:56 +1300

    Assignment

--- feb28.py
+++ feb28.py
@@ -9,6 +9,20 @@ q2 = "Create two new variables name and age that hold the name and age of Kabir 
 """Write your answer below here"""
 
 # Usman
+sentence = "my name is usman, i am 15 years old."
+def decapitalize(sentance):
+    return sentance[:0].upper()+sentance[3:]
+print(decapitalize("my name is usman, i am 15 years old."))
+
+name = "Kabir"
+Age = 15
+name , Age = Age, name
+print(Age)
+
+name = "Kabir"
+Age = 15
+name , Age = Age, name
+print(name)
 """Write your answer below here"""
 
 # Kassandrah
