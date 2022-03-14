ac1471a3a09b03ab236d58ff558803d746d74f67
Nasir Mustapha <teboy.com@gmail.com>
2022-03-14 09:59:41 +0100

    learning python operators

---
+++ march14.py
@@ -0,0 +1,13 @@
+"""Python Operators"""
+
+# Assignment Operators
+# =, +=, -=, *=, /=, %=, **=, //=
+
+# Logical Operators
+# and, or, not
+
+# Membership Operators
+# in, not in
+
+# Identity Operators
+# is, is not
