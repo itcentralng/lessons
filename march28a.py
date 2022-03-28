654bc8e177b4a07a90e2b1e68c5abb14f2d8f234
Nasir Mustapha <teboy.com@gmail.com>
2022-03-28 11:14:11 +0100

    march28 correctionsa nd assignment

---
+++ march28a.py
@@ -0,0 +1,62 @@
+"""Assignment"""
+
+languages = [
+  {"name":"Hausa", "population":1000, "continent":"Africa", "country":"Nigeria"},
+  {"name":"English", "population":7000, "continent":"Europe", "country":"England"},
+  {"name":"Arabic", "population":1200, "continent":"Asia", "country":"Saudi Arabia"},
+  {"name":"French", "population":2300, "continent":"Europe", "country":"France"},
+  {"name":"Spanish", "population":1500, "continent":"Europe", "country":"Spain"}
+]
+
+"""
+Using the language list above, answer the follwoing questions:
+1 - Write a code that prints out all the languages in a table with their names, 
+	population, continent and country in format as below:
+|Names|Population|Continent|Country|
+|Hausa|  1000    |  Africa |Nigeria|
+|Hausa|  1000    |  Africa |Nigeria|
+|Hausa|  1000    |  Africa |Nigeria|
+|Hausa|  1000    |  Africa |Nigeria|
+
+2. Create a table and prints out all the languages that have at least 2000 speakers
+3. Create a table and prints out all the Eurpean languages
+NOTE all tables must have this heading: |Names|Population|Continent|Country|
+"""
+
+print("CORRECTION")
+
+# Q1:
+print("TABLE SHOWING ALL LANGUAGES")
+table = f"|{'Name':<10}|{'Population':<10}|{'Continent':<10}|{'Country':<15}|\n"
+for language in languages:
+    name = language['name']
+    population = language['population']
+    continent = language['continent']
+    country = language['country']
+    table += f"|{name:<10}|{population:<10}|{continent:<10}|{country:<15}|\n"
+
+print(table)
+
+# Q2:
+print("TABLE SHOWING LANGUAGES WITH AT LEAST 2000 SPEAKERS")
+table2 = f"|{'Name':<10}|{'Population':<10}|{'Continent':<10}|{'Country':<15}|\n"
+for language in languages:
+    name = language['name']
+    population = language['population']
+    continent = language['continent']
+    country = language['country']
+    if population >= 2000:
+        table2 += f"|{name:<10}|{population:<10}|{continent:<10}|{country:<15}|\n"
+print(table2)
+
+#Q3:
+print("TABLE SHOWING EUROPEAN LANGUAGES")
+table3 = f"|{'Name':<10}|{'Population':<10}|{'Continent':<10}|{'Country':<15}|\n"
+for language in languages:
+    name = language['name']
+    population = language['population']
+    continent = language['continent']
+    country = language['country']
+    if continent == 'Europe':
+        table3 += f"|{name:<10}|{population:<10}|{continent:<10}|{country:<15}|\n"
+print(table3)
